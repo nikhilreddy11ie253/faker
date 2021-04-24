63a02ff103b251bb6b67f5a933bac3676ca848fb
root <root@DESKTOP-61A7F1R.localdomain>
2021-04-24 21:59:27 +0530

    add fake

--- index.js
+++ index.js
@@ -2,3 +2,38 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
+// faker.js fake date for laptops
+
+* @namespace faker.laptop
+*/
+var Laptop = function (faker) {
+ var self = this;
+ var fake = faker.fake;
+
+ /**
+  * manufacturer
+  *
+  * @method faker.laptop.manufacturer
+  */
+ self.manufacturer = function () {
+   return faker.random.arrayElement(faker.definitions.laptop.manufacturer);
+ };
+
+ self.manufacturer.schema = {
+   "description": "Generates a manufacturer name.",
+   "sampleResults": ["dell", "hp", "lenovo", "asus"]
+ };
+
+module["exports"] = Laptop;
+
+// * model
+a
+self.model = function () {
+    return faker.random.arrayElement(faker.definitions.laptop.model);
+  };
+
+  self.model.schema = {
+    "description": "Generates a laptop model.",
+    "sampleResults": ["i3", "i5", "i7", "i9"]
+  };
+  module["exports"] = model;
